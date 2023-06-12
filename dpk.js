a26aaccd86e0a4dccc068616e59e26891225858d
leodaiub <leodaiub@gmail.com>
2023-06-12 23:05:22 +0200

    Refactor string check and JSON stringify to String()

--- dpk.js
+++ dpk.js
@@ -1,4 +1,4 @@
-const crypto = require("crypto");
+import crypto from "crypto";
 
 export const createHash = (data) =>
   crypto.createHash("sha3-512").update(data).digest("hex");
@@ -15,15 +15,9 @@ export const deterministicPartitionKey = (event) => {
     candidate = createHash(data);
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = createHash(candidate);
   }
-  return candidate;
+
+  return String(candidate);
 };
